44a10e1ee24c563e0378c4ba5034f7c54e7b3ac0
christaborium <dxdstudio@gmail.com>
2018-03-03 01:17:36 -0800

    Add initial data options for more advanced schema values.

--- plantstuff/sources/plantsdb/scraper.py
+++ plantstuff/sources/plantsdb/scraper.py
@@ -1,6 +1,10 @@
 """Plantsdb.xyz "scraper"."""
 
+from collections import defaultdict
+
 import requests
+
+from plantstuff.core import fetch
 from plantstuff.core.cache import cache_json
 
 API_URL = 'https://plantsdb.xyz/search?symbol={symbol}'
@@ -8,6 +12,23 @@ USDA_PLANTLIST_URL = (
     'https://www.plants.usda.gov/java/downloadData?'
     'fileName=plantlst.txt&static=true'
 )
+get_dom = fetch.get_dom(directory='../../data/plantsdb')
+
+
+@cache_json(directory='../../data/plantsdb')
+def get_usda_options():
+    """Download all the options from USDA advanced form."""
+    url = 'https://plants.usda.gov/adv_search.html'
+    data, dom = get_dom(url)
+    sels = dom.find('#advqry').find('select')
+    data = defaultdict(list)
+    for sel in sels:
+        name = sel.get('name').lower().replace(' ', '')
+        for opt in sel.findall('option'):
+            data[name].append(opt.text)
+    from pprint import pprint as ppr
+    ppr(data)
+    return data
 
 
 @cache_json(directory='../../data/plantsdb')
@@ -55,4 +76,5 @@ def get_all_data_by_symbol():
 
 
 if __name__ == '__main__':
-    get_all_data_by_symbol()
+    # get_all_data_by_symbol()
+    get_usda_options()
