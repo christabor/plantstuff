2c0bae64d815ff745c28e8329852f5de1bfdff2c
christaborium <dxdstudio@gmail.com>
2018-03-23 23:00:57 -0700

    Add missing foliage data.

--- plantstuff/db/foliage.py
+++ plantstuff/db/foliage.py
@@ -1,5 +1,62 @@
 """Foliage categories."""
 
-FOLIAGE_TYPES = []
-FOLIAGE_MORPHOLOGY_TYPES = []
-FOLIAGE_ARRANGEMENT_TYPES = []
+FOLIAGE_TYPES = [
+    "brevideciduous",
+    "deciduous",
+    "evergreen",
+    "broadleaf-evergreen",
+    "marcescence"
+]
+FOLIAGE_ARRANGEMENT_TYPES = [
+    'whorled',
+    'alternate',
+    'spiral',
+    'opposite',
+]
+FOLIAGE_MORPHOLOGY_TYPES = [
+    # Simple
+    "ovate",
+    "linear",
+    "peltate",
+    "hastate",
+    "cordate",
+    "reniform",
+    "orbiculate",
+    "spatulate",
+    "lanceolate",
+    "undulate",
+    "sinuate",
+    "serrate",
+    "dentate",
+    "lobate",
+    "scalloped",
+    "palmate",
+    "digitate",
+    "palmapartite",
+    "pinnapartite",
+    "pinnatifid",
+    # Compound-pinnate
+    "unipinnate",
+    "geminate-pinnate",
+    "tetrapinnate",
+    "tripinnate",
+    "bipinnate",
+    "paripinnate",
+    "imparipinnate",
+    "alternipinnate",
+    "pinnately-lobed",
+    "pinnately-cleft",
+    "pinnately-cleft",
+    "pinnately-divided",
+    "pinnately-trifoliate",
+    "palmately-trifoliate",
+    "biternate",
+    "palmatisect",
+    "palmatibolate",
+    "pedate",
+    "bipartite",
+    "tripartite",
+    "bipinnatisect",
+    "tripinnatisect",
+    "pinnatisect",
+]
