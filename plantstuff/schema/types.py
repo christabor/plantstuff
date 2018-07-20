c0d3202ef6d364f6f2a81e3bbe673da361d08336
christaborium <dxdstudio@gmail.com>
2018-07-20 16:33:52 -0700

    stuff

--- plantstuff/schema/types.py
+++ plantstuff/schema/types.py
@@ -14,27 +14,40 @@ class BaseType(object):
     """Subclass that allows field agnostic metadata."""
 
     _sources = []
+    _type = None
 
 
 class StringType(BaseType, BaseStringType):
     """A wrapper subclass."""
 
+    _type = str
+
 
 class FloatType(BaseType, BaseFloatType):
     """A wrapper subclass."""
+
+    _type = float
 
 
 class BooleanType(BaseType, BaseBooleanType):
     """A wrapper subclass."""
 
+    _type = bool
+
 
 class ModelType(BaseType, BaseModelType):
     """A wrapper subclass."""
+
+    _type = object
 
 
 class ListType(BaseType, BaseListType):
     """A wrapper subclass."""
 
+    _type = list
+
 
 class IntType(BaseType, BaseIntType):
     """A wrapper subclass."""
+
+    _type = int
